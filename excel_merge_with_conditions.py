adf571236d6c60e0f624b751af63a15ac307f8dd
peterhe2004 <37344579+peterhe2004@users.noreply.github.com>
2023-06-24 15:50:41 -0400

    Update excel_merge_with_conditions.py

--- excel_merge_with_conditions.py
+++ excel_merge_with_conditions.py
@@ -19,4 +19,22 @@ df.loc[rows_to_copy, 'A'] = df.loc[rows_to_copy - 1, 'A'].values
 print(df)
 
 
+#<<Another way to achieve it>>
+import pandas as pd
 
+# Load the Excel file into a pandas DataFrame
+df = pd.read_excel('your_file.xlsx')
+
+# Define the maximum line number
+max_line_number = 10  # Adjust this value as per your requirement
+
+# Identify the rows that meet the conditions within the specified range
+condition1 = (df['A'].notnull() & df.iloc[:, 1:].isnull().all(axis=1)).astype(int)
+condition2 = (df['A'].isnull() & df.iloc[:, 1:].notnull().any(axis=1)).astype(int)
+condition2_shifted = condition2.shift(-1)
+
+# Copy the content from column 'A' of the 1st line to column 'A' of the 2nd line
+df.loc[condition2_shifted.astype(bool) & (df.index <= max_line_number), 'A'] = df.loc[condition1.astype(bool), 'A'].values
+
+# Display the updated DataFrame
+print(df)
