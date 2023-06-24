84c64326f866d517f67b1fac33d7ca3d36c5cf5e
peterhe2004 <37344579+peterhe2004@users.noreply.github.com>
2023-06-24 17:45:50 -0400

    Update excel_merge_with_conditions.py

--- excel_merge_with_conditions.py
+++ excel_merge_with_conditions.py
@@ -77,11 +77,15 @@ df = pd.read_excel('shCDP.xlsx')
 # Define the maximum line number
 max_line_number = 10  # Adjust this value as per your requirement
 
-# Iterate over the rows of the DataFrame
-for i in range(1, len(df)):
+# Iterate over the rows of the DataFrame in reverse order
+for i in range(len(df)-1, 0, -1):
     # Check the conditions for copying
     if pd.isna(df.at[i, 'A']) and not pd.isna(df.at[i-1, 'A']) and df.loc[i, 'B':'E'].notnull().any():
         df.at[i, 'A'] = df.at[i-1, 'A']  # Copy the content from cell A1 to A2
+        df = df.drop(i-1)  # Delete the row i-1
+
+# Reset the index after deleting rows
+df = df.reset_index(drop=True)
 
 # Save the updated DataFrame to a new Excel file
 df.to_excel('shCDP_v1.xlsx', index=False)
