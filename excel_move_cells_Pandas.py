a90918b8174790a11841b4e3d8f7ea3b28900ea3
peterhe2004 <phecisco@gmail.com>
2023-07-19 14:06:45 -0400

    fixed iloc[:, 2] to be iloc[i:, 2]; and used pd.isna to check 'None'

--- excel_move_cells_Pandas.py
+++ excel_move_cells_Pandas.py
@@ -1,19 +1,28 @@
 import pandas as pd
+import numpy as np
 
 # Read the Excel file into a DataFrame
-df = pd.read_excel('pdtest1.xlsx', sheet_name='Sheet1')
+df = pd.read_excel('pdtest1.xlsx', sheet_name='Sheet1', header=None)
+print(df.head())
 
 # Define the column indices you want to move
 column_indices = [4, 3, 2]  # Replace with the desired column indices
 
-# Iterate over the column indices and move the cells
-for i in range (len(df)):
-    condition = df.iloc[i, 2] is None
-    if condition:
-            print(condition)
-            for col_idx in column_indices:
-                df.iloc[:, col_idx + 2] = df.iloc[:, col_idx].astype(str)
-                df.iloc[:, col_idx] = None
+# Add additional empty columns if necessary
+max_idx = max(column_indices)
+if max_idx + 2 >= df.shape[1]:
+     for i in range(df.shape[1], max_idx + 3):
+          df[i] = np.nan
 
+
+# Iterate over the column indices and move the cells
+for i in range(df.shape[0]):
+    condition = pd.isna(df.iloc[i, 2])
+    if condition:
+            print(f'Row {i+1} column 3 is NaN: {condition}')
+            for col_idx in column_indices:
+                df.iloc[i, col_idx + 2] = df.iloc[i, col_idx]
+                df.iloc[i, col_idx] = np.nan
+ 
 # Save the modified DataFrame to Excel
 df.to_excel('pdtest2.xlsx', index=False)
