cb0269ec4e540bd0589cc6f9b7448a9f05c6e3b4
peterhe2004 <phecisco@gmail.com>
2023-08-30 00:17:41 -0400

    mapping py and j2

---
+++ port_mapping_v1.py
@@ -0,0 +1,20 @@
+from jinja2 import Environment, FileSystemLoader
+import csv
+import os
+
+# Read CSV file into a list of dictionaries
+with open('shCDP5.csv', 'r') as f:
+    reader = csv.DictReader(f)
+    data = [row for row in reader]
+
+# Set up Jinja2 environment and load the template
+env = Environment(loader=FileSystemLoader('.'))
+template = env.get_template('port_mapping_plan.j2')
+
+# Render the template
+output = template.render(data=data)
+e = open(os.path.join("testMapping.csv"), 'w')
+e.write(output)
+
+# Print the output
+print(output)
